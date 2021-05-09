78da4eaec0963491351346e6b1956cf64a1e019a
bryanjug <bryandjug@gmail.com>
2021-05-08 18:55:32 -0700

    changed port and npm start

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+const port = process.env.PORT || 4000;
+
 server.use(middlewares)
 server.use(router)
-server.listen(3001, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
 
